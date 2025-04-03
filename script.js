fc0cce668c736806c58185829b745c9cdc99fa4b
Aaron Dominguez <onlinebusinessfy@gmail.com>
2025-04-03 11:55:33 -0700

    Avance en clase 2

--- script.js
+++ script.js
@@ -8,13 +8,13 @@ function agregarTabla(){
     productos.forEach(producto => {
         const fila = document.createElement("tr");
         fila.innerHTML=`
-        <td>${producto.id}<td>
+        <td>${producto.id}</td>
         <td contenteditable="true" onBlur="updateProduct(${producto.id}, 'nombre', this.innerText)">${producto.nombre}</td>
         <td contenteditable="true" onBlur="updateProduct(${producto.id}, 'precio', this.innerText)">${producto.precio}</td>
         <td contenteditable="true" onBlur="updateProduct(${producto.id}, 'cantidad', this.innerText)">${producto.cantidad}</td>
         <td><button onclick="eliminarProducto(${producto.id})">Eliminar</button></td>
         `;
-        tableBody.appendChild(row);
+        tabla.appendChild(row);
     });
 }
 
@@ -23,20 +23,20 @@ document.getElementById("FormularioProductos").addEventListener("submit", functi
     const nombre = document.getElementById("nombre").value;
     const precio = parseFloat(document.getElementById("precio").value);
     const cantidad = parseInt(document.getElementById("cantidad").value);
-    products.push({ id: nextId++, nombre, precio, cantidad });
+    productos.push({ id: nextId++, nombre, precio, cantidad });
     renderTable();
     this.reset();
 });
 
 function actualizarProducto(id, campo, valor) {
-    const producto = products.find(p => p.id === id);
+    const producto = productos.find(p => p.id === id);
     if (producto) {
         producto[campo] = campo === "nombre" ? valor : parseFloat(valor);
     }
   }
 
 function eliminarProducto(id) {
-    products = products.filter(p => p.id !== id);
+    productos = productos.filter(p => p.id !== id);
     renderTable();
 }
 
